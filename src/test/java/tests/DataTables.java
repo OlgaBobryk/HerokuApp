1e613d3861056d3f5b820a3b4a557980a4bb3557
OlgaBobryk <oljunja-86@mail.ru>
2022-07-19 23:15:44 +0300

    DataTables.java changed

--- src/test/java/tests/DataTables.java
+++ src/test/java/tests/DataTables.java
@@ -13,9 +13,9 @@ public class DataTables extends BaseTest {
         driver.get(Urls.DATA_TABLES_URL);
         //Add check
         driver.findElement(By.xpath("//table[@id='table1']//td[text()='fbach@yahoo.com']"));
-        WebElement elementLastName = driver.findElement(By.xpath("//table[@id='table1']//td[text()='fbach@yahoo.com']//preceding-sibling::td[contains(text(),'Bach')]"));
+        WebElement elementLastName = driver.findElement(By.xpath("//table[@id='table1']//td[text()='fbach@yahoo.com']//preceding-sibling::td[2]"));
         String lastNameActual = elementLastName.getText();
-        WebElement elementFirstName = driver.findElement(By.xpath("//table[@id='table1']//td[text()='fbach@yahoo.com']//preceding-sibling::td[contains(text(),'Frank')]"));
+        WebElement elementFirstName = driver.findElement(By.xpath("//table[@id='table1']//td[text()='fbach@yahoo.com']//preceding-sibling::td[1]"));
         String firstNameActual = elementFirstName.getText();
         Assert.assertEquals("Bach", lastNameActual, "There isn't this Last Name");
         Assert.assertEquals("Frank", firstNameActual, "There isn't this First Name");
@@ -27,9 +27,9 @@ public class DataTables extends BaseTest {
         driver.get(Urls.DATA_TABLES_URL);
         //Add check
         driver.findElement(By.xpath("//table[@id='table1']//td[text()='fbach@yahoo.com']"));
-        WebElement elementDue=driver.findElement(By.xpath("//table[@id='table1']//td[text()='fbach@yahoo.com']//following-sibling::td[contains (text(), '$51')]"));
+        WebElement elementDue=driver.findElement(By.xpath("//table[@id='table1']//td[text()='fbach@yahoo.com']//following-sibling::td[contains (text(), '$')]"));
         String dueActual=elementDue.getText();
-        WebElement elementEmail=driver.findElement(By.xpath("//table[@id='table1']//td[text()='fbach@yahoo.com']//following-sibling::td[contains (text(), 'http://www.frank.com')]"));
+        WebElement elementEmail=driver.findElement(By.xpath("//table[@id='table1']//td[text()='fbach@yahoo.com']//following-sibling::td[contains (text(), 'www')]"));
         String emailActual=elementEmail.getText();
         Assert.assertEquals("$51.00",dueActual,"There isn't this text");
         Assert.assertEquals("http://www.frank.com", emailActual, "There isn't this email");
